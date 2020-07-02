43ddb8fd53b0d18ca81ce0d2e1c9966745c61a7c
Kris <kristapsk@gmail.com>
2020-07-02 02:03:14 +0100

    add html

--- website/website/settings.py
+++ website/website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
+    'frontpage.apps.FrontpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
