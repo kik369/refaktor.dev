f7c67f501c5f840d8390dd6e6f644996c1ea36a6
Kris <kristapsk@gmail.com>
2020-07-13 23:36:31 +0100

    update frontpage

--- website/frontpage/views.py
+++ website/frontpage/views.py
@@ -1,12 +1,14 @@
 from django.views import generic
 from django.http import HttpResponse
 
+
 class HomeView(generic.TemplateView):
     template_name = 'frontpage/home.html'
 
 
 def robots(request):
-    response = HttpResponse('User-agent: *\nSitemap: http://refaktor.dev/sitemap.xml', content_type='text/plain')
+    response = HttpResponse(
+        'User-agent: *\nSitemap: http://refaktor.dev/sitemap.xml', content_type='text/plain')
     return response
 
 
