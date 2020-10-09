acd4aa32c9c25133eed75330e6324ee84a0db6de
Kris <kristapsk@gmail.com>
2020-10-09 21:03:57 +0100

    bug fixes and performance improvements :)

--- website/frontpage/static/frontpage/js/clock.js
+++ website/frontpage/static/frontpage/js/clock.js
@@ -1,18 +1,18 @@
-const clock = document.querySelector('.clock')
-const body = document.querySelector('body')
+const clock = document.querySelector('.clock');
+const body = document.querySelector('body');
 
 function addZero(time) {
-    if (time < 10) {
-        time = '0' + time;
-    }
-    return time;
+  if (time < 10) {
+    time = '0' + time;
+  }
+  return time;
 }
 
 setInterval(() => {
-    let now = new Date()
-    let hour = addZero(now.getHours())
-    let minute = addZero(now.getMinutes())
-    let second = addZero(now.getSeconds())
-    body.style.backgroundImage = `linear-gradient(45deg, #${hour}${minute}${second}, #${second}${minute}${hour})`
-    clock.innerHTML = `${hour}:${minute}:${second}`
+  let now = new Date();
+  let hour = addZero(now.getHours());
+  let minute = addZero(now.getMinutes());
+  let second = addZero(now.getSeconds());
+  body.style.backgroundImage = `linear-gradient(45deg, #${hour}${minute}${second}, #${second}${minute}${hour})`;
+  clock.innerHTML = `${hour}:${minute}:${second}`;
 }, 1000);
