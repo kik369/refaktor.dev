c39ab0a07d97c7ef96c0ceedb8cea34669f9dfee
Kris Kringelis <kris@refaktor.dev>
2022-02-12 23:20:23 +0000

    change js clock

--- website/frontpage/static/frontpage/js/clock.js
+++ website/frontpage/static/frontpage/js/clock.js
@@ -2,17 +2,23 @@ const clock = document.querySelector('.clock');
 const body = document.querySelector('body');
 
 function addZero(time) {
-  if (time < 10) {
-    time = '0' + time;
-  }
-  return time;
+    if (time < 10) {
+        time = '0' + time;
+    }
+    return time;
 }
 
 setInterval(() => {
-  let now = new Date();
-  let hour = addZero(now.getHours());
-  let minute = addZero(now.getMinutes());
-  let second = addZero(now.getSeconds());
-  body.style.backgroundImage = `linear-gradient(45deg, #${hour}${minute}${second}, #${second}${minute}${hour})`;
-  clock.innerHTML = `${hour}:${minute}:${second}`;
+    let now = new Date();
+    let hour = addZero(now.getHours());
+    let minute = addZero(now.getMinutes());
+    let second = addZero(now.getSeconds());
+
+    let hour_col = parseInt((255 / 23) * hour);
+    let minute_col = parseInt((255 / 59) * minute);
+    let second_col = parseInt((255 / 59) * second);
+
+    body.style.backgroundImage = `linear-gradient(45deg, rgba(${hour_col}, ${minute_col}, ${second_col}, 0.75), rgba(${second_col}, ${minute_col}, ${hour_col}, 0.75))`;
+
+    clock.innerHTML = `${hour}:${minute}:${second}`;
 }, 1000);
--- website/static/frontpage/js/clock.js
+++ website/static/frontpage/js/clock.js
@@ -2,17 +2,28 @@ const clock = document.querySelector('.clock');
 const body = document.querySelector('body');
 
 function addZero(time) {
-  if (time < 10) {
-    time = '0' + time;
-  }
-  return time;
+    if (time < 10) {
+        time = '0' + time;
+    }
+    return time;
 }
 
 setInterval(() => {
-  let now = new Date();
-  let hour = addZero(now.getHours());
-  let minute = addZero(now.getMinutes());
-  let second = addZero(now.getSeconds());
-  body.style.backgroundImage = `linear-gradient(45deg, #${hour}${minute}${second}, #${second}${minute}${hour})`;
-  clock.innerHTML = `${hour}:${minute}:${second}`;
+    let now = new Date();
+    let hour = addZero(now.getHours());
+    let minute = addZero(now.getMinutes());
+    let second = addZero(now.getSeconds());
+
+    let hour_col = parseInt((255 / 23) * hour);
+    let minute_col = parseInt((255 / 59) * minute);
+    let second_col = parseInt((255 / 59) * second);
+
+    // body.style.backgroundImage = `linear-gradient(45deg, #${hour_col}${minute_col}${second_col}, #${second_col}${minute_col}${hour_col})`;
+
+    body.style.backgroundImage = `linear-gradient(45deg, rgba(${hour_col}, ${minute_col}, ${second_col}, 1), rgba(${second_col}, ${minute_col}, ${hour_col}, 1))`;
+
+    // body.style.backgroundImage = `linear-gradient(45deg, #${hour}${minute}${second}, #${second}${minute}${hour})`;
+    clock.innerHTML = `${hour}:${minute}:${second}`;
+
+    console.log(hour_col, minute_col, second_col);
 }, 1000);
